4cd611484263deb282e177f2f272fdabd1a35703
GGWithGitHub <ghogareg1996@gmail.com>
2023-08-19 21:27:10 +0530

    Worked on the "Logging To File Using NLOG Example".

---
+++ Learn_core_mvc/Controllers/LoggingController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Learn_core_mvc.Controllers
+{
+    public class LoggingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GenerateDivideByZeroError()
+        {
+            var a = 10;
+            var b = 0;
+            try
+            {
+                var c = a / b;
+            }
+            catch (Exception ex)
+            {
+                var file = @"F:\Gaurav_essential\ASP.NET CORE\Learn_core_mvc\Learn_core_mvc\AppLogs";
+                ViewBag.Msg = $"Error is logged on the following file - {file}";
+            }
+            return View("Index");
+        }
+
+        public IActionResult GenerateCustomError()
+        {
+            try
+            {
+                throw new Exception("I am custom generated error.");
+            }
+            catch (Exception ex)
+            {
+                var file = @"F:\Gaurav_essential\ASP.NET CORE\Learn_core_mvc\Learn_core_mvc\AppLogs";
+                ViewBag.Msg = $"Error is logged on the following file - {file}";
+            }
+            return View("Index");
+        }
+    }
+}
--- Learn_core_mvc/Program.cs
+++ Learn_core_mvc/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,9 +19,14 @@ namespace Learn_core_mvc
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            .ConfigureLogging((hostingContext, logging) => //Logging using nlog
+            {
+                // Enable NLog as one of the Logging Provider
+                logging.AddNLog();
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
