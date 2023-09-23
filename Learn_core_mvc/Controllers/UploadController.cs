b009f405242568022dcce5a865d0027b6373d34c
GGWithGitHub <ghogareg1996@gmail.com>
2023-09-23 20:59:25 +0530

    Worked on "Upload Image In WWWRoot Folder Example"

---
+++ Learn_core_mvc/Controllers/UploadController.cs
@@ -0,0 +1,48 @@
+﻿using Learn_core_mvc.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Learn_core_mvc.Controllers
+{
+    public class UploadController : Controller
+    {
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public UploadController(IWebHostEnvironment hostEnvironment)
+        {
+            _hostEnvironment = hostEnvironment;
+        }
+        public IActionResult UploadImage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateImage(ImageModel imageModel)
+        {
+            string wwwRootPath = _hostEnvironment.WebRootPath;
+            string fileName = Path.GetFileNameWithoutExtension(imageModel.ImageFile.FileName);
+            string extension = Path.GetExtension(imageModel.ImageFile.FileName);
+            imageModel.ImageName = fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+            var dir = Path.Combine(wwwRootPath, "UploadedImage");
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            var filePath = Path.Combine(dir, fileName);
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await imageModel.ImageFile.CopyToAsync(fileStream);
+            }
+
+            ViewBag.ImagePath = $"/UploadedImage/{fileName}";
+
+            return View("UploadImage");
+        }
+    }
+}
---
+++ Learn_core_mvc/Models/ImageModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Learn_core_mvc.Models
+{
+    public class ImageModel
+    {
+        public int ImageId { get; set; }
+        public string Title { get; set; }
+        public string ImageName { get; set; }
+        public IFormFile ImageFile { get; set; }
+    }
+}
