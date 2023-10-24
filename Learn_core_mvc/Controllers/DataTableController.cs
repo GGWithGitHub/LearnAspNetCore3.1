bbc2e338fff0492a275f28efadf23e0ef62d46e0
GGWithGitHub <ghogareg1996@gmail.com>
2023-10-24 23:05:23 +0530

    Worked on "Map Columns From DataTable To Properties Of A Model Class Example".

--- Learn_core_mvc/Controllers/DataTableController.cs
+++ Learn_core_mvc/Controllers/DataTableController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -104,6 +105,80 @@ namespace Learn_core_mvc.Controllers
                 recordsFiltered = totalRecords,
                 data = filteredData
             });
+        }
+
+        public IActionResult FillModelFromDataTable()
+        {
+            
+            DataTable dt = GetDataTable();
+            List<FillModelFromDt> models = dt.AsEnumerable().Select(row =>
+            {
+                FillModelFromDt model = new FillModelFromDt();
+                foreach (var property in typeof(FillModelFromDt).GetProperties())
+                {
+                    if (dt.Columns.Contains(property.Name))
+                    {
+                        var value = row[property.Name];
+                        if (value != DBNull.Value)
+                        {
+                            property.SetValue(model, Convert.ChangeType(value, property.PropertyType));
+                        }
+                    }
+                }
+                return model;
+            }).ToList();
+
+            return View(models);
+        }
+
+        private DataTable GetDataTable()
+        {
+            DataTable empTable = new DataTable("Employees");
+            DataColumn dtColumn;
+            DataRow myDataRow;
+
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(Int32);
+            dtColumn.ColumnName = "Id";
+            empTable.Columns.Add(dtColumn);
+
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(String);
+            dtColumn.ColumnName = "Name";
+            empTable.Columns.Add(dtColumn);
+
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(Decimal);
+            dtColumn.ColumnName = "Salary";
+            empTable.Columns.Add(dtColumn);
+
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(DateTime);
+            dtColumn.ColumnName = "JoinDate";
+            empTable.Columns.Add(dtColumn);
+
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(Boolean);
+            dtColumn.ColumnName = "IsQuit";
+            empTable.Columns.Add(dtColumn);
+
+            myDataRow = empTable.NewRow();
+            myDataRow["Id"] = 1001;
+            myDataRow["Name"] = "Harsh";
+            myDataRow["Salary"] = 15000.00;
+            myDataRow["JoinDate"] = Convert.ToDateTime("10/22/2015 12:10:15 PM");
+            myDataRow["IsQuit"] = false;
+            empTable.Rows.Add(myDataRow);
+
+            myDataRow = empTable.NewRow();
+            myDataRow["Id"] = 1002;
+            myDataRow["Name"] = "Gopal";
+            myDataRow["Salary"] = 20000.00;
+            myDataRow["JoinDate"] = Convert.ToDateTime("10/17/2018 10:10:15 AM");
+            myDataRow["IsQuit"] = true;
+            empTable.Rows.Add(myDataRow);
+
+            return empTable;
         }
     }
 }
---
+++ Learn_core_mvc/Models/DataTableModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Learn_core_mvc.Models
+{
+    public class FillModelFromDt
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime JoinDate { get; set; }
+        public bool IsQuit { get; set; }
+    }
+}
