7cbf1b25194396bd41d3704e4b62b3ce39728fc7
GGWithGitHub <ghogareg1996@gmail.com>
2023-10-29 22:48:31 +0530

    Worked on "Compress Image Example"

--- Learn_core_mvc/Controllers/UploadController.cs
+++ Learn_core_mvc/Controllers/UploadController.cs
@@ -1,6 +1,10 @@
 ﻿using Learn_core_mvc.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -43,6 +47,59 @@ namespace Learn_core_mvc.Controllers
             ViewBag.ImagePath = $"/UploadedImage/{fileName}";
 
             return View("UploadImage");
+        }
+
+        public IActionResult CompressImage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CompressImage(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                using (var stream = file.OpenReadStream())
+                using (var image = await Image.LoadAsync(stream))
+                {
+                    var quality = 50;
+
+                    // Save the compressed image
+                    var uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+                    var filePath = Path.Combine("wwwroot/images", uniqueFileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await image.SaveAsync(fileStream, new JpegEncoder { Quality = quality });
+                    }
+
+                    byte[] imageBytes = ConvertImageToByteArray(filePath);
+
+                    // Set the file content type
+                    var contentType = "image/jpeg"; // Change this according to your image type
+
+                    // Set the file name (you can customize this)
+                    var fileName = "compressed_image.jpg";
+
+                    // Send the file to the browser
+                    return File(imageBytes, contentType, fileName);
+                }
+            }
+            return View("CompressImage");
+        }
+
+        public byte[] ConvertImageToByteArray(string filePath)
+        {
+            // Check if the file exists
+            if (!System.IO.File.Exists(filePath))
+            {
+                throw new FileNotFoundException("File not found.", filePath);
+            }
+
+            // Read the file as bytes
+            byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+
+            return fileBytes;
         }
     }
 }
