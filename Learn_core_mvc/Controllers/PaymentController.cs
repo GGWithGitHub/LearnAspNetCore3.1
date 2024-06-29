e2cc39870024a6a0030303e312269967727a58e3
GGWithGitHub <ghogareg1996@gmail.com>
2024-06-29 18:30:59 +0530

    Payment Using Stripe Example

---
+++ Learn_core_mvc/Controllers/PaymentController.cs
@@ -0,0 +1,151 @@
+﻿using Learn_core_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Stripe;
+using Stripe.Checkout;
+using Microsoft.Extensions.Configuration;
+
+namespace Learn_core_mvc.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public PaymentController(IConfiguration configuration)
+        {
+            StripeConfiguration.ApiKey = configuration.GetValue<string>("Stripe:Secret");
+        }
+        public IActionResult StripeCheckout()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult CheckEmailExists(string email)
+        {
+            if (email == "abc@gmail.com" || email == "xyz@gmail.com")
+            {
+                return Json(false);
+            }
+            return Json(true);
+        }
+
+        public IActionResult CheckStripeUserForm(StripeUserFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return Json(new { success = true });
+            }
+            else
+            {
+                // Collect all error messages from ModelState
+                var errors = ModelState.Values.SelectMany(v => v.Errors)
+                                              .Select(e => e.ErrorMessage)
+                                              .ToList();
+
+                return Json(new { success = false, errors });
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CreateCheckoutSession(
+            string firstName,
+            string lastName,
+            string email,
+            string password,
+            string confirmPassword,
+            string smsPhone,
+            string address1,
+            string city,
+            string state,
+            string country,
+            int subscriptionTypeId
+        )
+        {
+            try
+            {
+                var customParameters = new Dictionary<string, string>
+            {
+                { "firstName", firstName },
+                { "lastName", lastName },
+                { "email", email },
+                { "password", password },
+                { "smsPhone", smsPhone },
+                { "address1", address1 },
+                { "city", city },
+                { "state", state },
+                { "country", country },
+                { "subscriptionTypeId", subscriptionTypeId.ToString() }
+            };
+
+                var baseUrl = $"{Request.Scheme}://{Request.Host}";
+                var options = new SessionCreateOptions
+                {
+                    PaymentMethodTypes = new List<string> { "card" },
+                    //BillingAddressCollection = "required",
+                    CustomerEmail = email,
+                    LineItems = new List<SessionLineItemOptions>
+                    {
+                        new SessionLineItemOptions
+                        {
+                            PriceData = new SessionLineItemPriceDataOptions
+                            {
+                                Currency = "usd",
+                                ProductData = new SessionLineItemPriceDataProductDataOptions
+                                {
+                                    Name = "Product name",
+                                },
+                                UnitAmount = 10 * 100, // amount in cents
+							},
+                            Quantity = 1,
+                        },
+                    },
+                    Metadata = customParameters, // Add custom parameters to metadata
+                    Mode = "payment",
+                    SuccessUrl = baseUrl + "/Payment/StripeSuccess?sessionId={CHECKOUT_SESSION_ID}",
+                    CancelUrl = $"{baseUrl}/Payment/StripeCancel",
+                };
+
+                var service = new SessionService();
+                var session = service.Create(options);
+
+                options.SuccessUrl = options.SuccessUrl.Replace("{CHECKOUT_SESSION_ID}", session.Id);
+
+                return Json(new { success = true, sessionId = session.Id });
+            }
+            catch (Exception ex)
+            {
+                var error = ex.Message ?? ex.InnerException.Message;
+                return Json(new { success = false, error });
+            }
+        }
+
+        [HttpGet]
+        public ActionResult StripeSuccess(string sessionId)
+        {
+            var service = new SessionService();
+            var session = service.Get(sessionId);
+
+            var firstName = session.Metadata["firstName"];
+            var lastName = session.Metadata["lastName"];
+            var email = session.Metadata["email"];
+            var password = session.Metadata["password"];
+            var smsPhone = session.Metadata["smsPhone"];
+            var address1 = session.Metadata["address1"];
+            var city = session.Metadata["city"];
+            var state = session.Metadata["state"];
+            var country = session.Metadata["country"];
+            var subscriptionTypeId = session.Metadata["subscriptionTypeId"];
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult StripeCancel()
+        {
+            return View();
+        }
+    }
+}
---
+++ Learn_core_mvc/Models/PaymentModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Learn_core_mvc.Models
+{
+    public class StripeUserFormModel
+    {
+        [Required(ErrorMessage = "FirstName is required.")]
+        [MinLength(3,ErrorMessage = "FirstName should have at least 3 character.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName is required.")]
+        [MinLength(3, ErrorMessage = "LastName should have at least 3 character.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Email is not valid.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{6,}$", ErrorMessage = "Password must contain at least one digit, one lowercase letter, one uppercase letter, one special character, and be at least 6 characters long.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "ConfirmPassword is required.")]
+        [Compare("Password", ErrorMessage = "Password and confirm password must be same.")]
+        public string ConfirmPassword { get; set; }
+
+        public string SmsPhone { get; set; }
+
+        [Required(ErrorMessage = "Address1 is required.")]
+        public string Address1 { get; set; }
+
+        [Required(ErrorMessage = "City is required.")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "State is required.")]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "Country is required.")]
+        public string Country { get; set; }
+        public int SubscriptionTypeId { get; set; }
+    }
+}
